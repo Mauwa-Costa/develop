e9cd404ed4d26278e5fa13544be02a920b6952ab
Mauwa Costa <104799224+Mauwa-Costa@users.noreply.github.com>
2022-08-26 16:08:50 +0100

    develop

--- functions/url.php
+++ functions/url.php
@@ -2,6 +2,7 @@
 
 function url_redirect($values = []) {
     $buildQueryString = http_build_query($values);
-    header('Location: http://localhost/?' . $buildQueryString);
+    header('Location: http://localhost/http://localhost:8888/exercicios/Exercicio%2003%20-%20Minha%20primeira%20aplicacao:sistema%20em%20PHP/?' . $buildQueryString);
+
     exit; 
 }
